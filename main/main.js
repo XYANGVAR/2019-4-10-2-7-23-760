246c13dba4b393d6e9b13aa8b4e0b174a414e09c
XYANGVAR <1049372609@qq.com>
2019-05-28 09:41:25 +0800

    first

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+   var distance,parkTime,price,price1;
+   distance=inputs.distance;
+   parkTime=inputs.parkTime;
+   if(distance<2)
+	   price1=6+0.25*parkTime;
+   else if(distance<=8)
+   {
+	   price1=6+0.25*parkTime+0.8*(distance-2);
+   }
+   else 
+   {
+	   price1=6+0.25*parkTime+0.8*6+1.2*(distance-8);
+   }
+   price=Math.round(price1);
+    return price;
 };
